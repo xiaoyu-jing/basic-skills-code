1769999e13f5f00be2772aa0a858a8cb758b62c4
jing1560 <363102403@qq.com>
2024-04-03 18:51:21 +0800

    链表向右循环K个数

---
+++ src/main/java/com/tech/solution/leetcode/LinkedListCycleKth.java
@@ -0,0 +1,112 @@
+package com.tech.solution.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jing1560
+ * @data 2024/4/3
+ *
+ * 字节 算法：链表向右循环K个数
+ *
+ * 示例：
+ * 输入：node = [2,3,-2,4]  k = 2
+ * 输出：node = [-2,4,2,3]
+ */
+public class LinkedListCycleKth {
+
+    public static class LinkedNode {
+        int value;
+        LinkedNode next;
+        public LinkedNode(int _value){
+            this(_value,null);
+        }
+        public LinkedNode(int _value, LinkedNode _next){
+            this.value = _value;
+            this.next = _next;
+        }
+
+        public String toString(){
+            StringBuilder builder = new StringBuilder();
+            LinkedNode tempNode = this;
+            while (tempNode != null){
+                if(builder.length() > 0){
+                    builder.append(",");
+                }
+                builder.append(tempNode.value);
+                tempNode = tempNode.next;
+            }
+            return builder.toString();
+        }
+    }
+
+    public static void main(String[] args){
+        LinkedNode node = initNode(new int[]{2,3,-2,4}, 0);
+        System.out.println(cycleKthSolution1(node, 2).toString());
+    }
+
+    /**
+     * 初始化节点数据
+     * @param nums
+     * @param count
+     * @return
+     */
+    private static LinkedNode initNode(int[] nums, int count){
+        if(count == nums.length){
+            return null;
+        }
+        LinkedNode initNode = new LinkedNode(nums[count]);
+        initNode.next = initNode(nums, count + 1);
+        return initNode;
+    }
+
+    private static LinkedNode cycleKthSolution1(LinkedNode node, int k){
+
+        List<Integer> list = new ArrayList<>();
+        LinkedNode currentNode = node;
+        while (currentNode != null){
+            list.add(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        if(list.size() <= 0 || k <= 0){
+            return node;
+        }
+        List<Integer> newList = new ArrayList<>(list.size());
+        // 此处必须进行一次 list 的转换（在同一个list上既遍历又修改，会报错ConcurrentModificationException），用 list 遍历，用 newList 修改值
+        newList.addAll(list);
+        // 第一层循环表示 移动的轮次，移动 k 轮，也就是 向右移动 k 个数
+        for(int i = 0; i < k; i++){
+            int tailTemp = 0;
+            // 第二层循环 用来向右移动数字
+            for(int j = list.size() - 1; j >=-1; j--){
+                if(j == list.size() - 1){
+                    // tailTemp 用来存储最后一位数字
+                    tailTemp = list.get(list.size()-1);
+                    continue;
+                }
+                if(j == -1){
+                    // 将最后一位数字 塞到 list的头部
+                    newList.set(j + 1, tailTemp);
+                    continue;
+                }
+                newList.set(j + 1, list.get(j));
+            }
+            list = newList;
+        }
+
+        //处理 list，组装 Node
+        LinkedNode newNode = buildLinkedNode(list,0);
+
+        return newNode;
+    }
+
+    private static LinkedNode buildLinkedNode(List<Integer> newList, int count){
+        if(count == newList.size()){
+            return null;
+        }
+        LinkedNode newNode = new LinkedNode(newList.get(count));
+        newNode.next = buildLinkedNode(newList, count + 1);
+
+        return newNode;
+    }
+}
