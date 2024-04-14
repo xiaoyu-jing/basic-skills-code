9e85b3eaa78a27e5679417dc2801d54f798a40fe
jing1560 <363102403@qq.com>
2024-04-14 22:51:38 +0800

    Jump Game [can jump]

--- src/main/java/com/tech/solution/leetcode/JumpGame.java
+++ src/main/java/com/tech/solution/leetcode/JumpGame.java
@@ -31,7 +31,7 @@ public class JumpGame {
         int max_far = 0;
         // 最远右边界
         int end = 0;
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < nums.length - 1; i++){
             max_far = Math.max(max_far, i + nums[i]);
             if(end == i){
                 end = max_far;
