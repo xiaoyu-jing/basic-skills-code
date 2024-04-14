3a50d55f4bf006fa99793d554d245ac332ae6d90
jing1560 <363102403@qq.com>
2024-04-14 23:25:23 +0800

    Build Tree

--- src/main/java/com/tech/solution/leetcode/BuildTreeSolution2.java
+++ src/main/java/com/tech/solution/leetcode/BuildTreeSolution2.java
@@ -8,13 +8,23 @@ import java.util.Map;
  * @data 2024/2/8
  *
  * 算法：从前序与中序遍历序列构造二叉树
+ *
+ * 给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
+ *
+ *  示例 1:
+ *  输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+ *  输出: [3,9,20,null,null,15,7]
+ *
+ *  示例 2:
+ *  输入: preorder = [-1], inorder = [-1]
+ *  输出: [-1]
  */
 public class BuildTreeSolution2 {
 
-    public static int rootIndex = 0;
-
     public static void main(String[] args){
-        buildTree();
+        int[] preOrder = {3,9,20,15,7};
+        int[] inOrder = {9,3,15,20,7};
+        System.out.println(buildTree(preOrder,inOrder));
     }
 
     private static class TreeNode {
@@ -32,32 +42,29 @@ public class BuildTreeSolution2 {
         }
     }
 
-    private static void buildTree(){
-        int[] preOrder = {3,9,20,15,7};
-        int[] inOrder = {9,3,15,20,7};
-
+    private static TreeNode buildTree(int[] preOrder, int[] inOrder){
         if(preOrder == null || inOrder == null || preOrder.length != inOrder.length){
-            return;
+            return null;
         }
-
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i=0; i<inOrder.length; i++){
-            map.put(inOrder[i],i);
+        // 中序遍历作为标准，放在Map中
+        Map<Integer,Integer> inMap = new HashMap<>();
+        for(int i = 0; i < inOrder.length; i++){
+            inMap.put(inOrder[i], i);
         }
-
-        TreeNode node = buildTree(map,0,preOrder.length-1,preOrder);
-        System.out.println(node);
+        TreeNode root = buildTree(inMap,0,preOrder.length - 1, preOrder);
+        return root;
     }
 
-    private static TreeNode buildTree(Map<Integer,Integer> map, int left, int right, int[] preOrder){
+    public static int rootIndex = 0;
+
+    private static TreeNode buildTree(Map<Integer,Integer> inMap, int left, int right, int[] preOrder){
         if(left <= right){
             int rootValue = preOrder[rootIndex];
             TreeNode root = new TreeNode(rootValue);
             // rootIndex为根节点计数器，由于是先序遍历，所以每次+1，表示取到的就是根节点
             rootIndex++;
-            root.left = buildTree(map,left, map.get(rootValue) -1, preOrder);
-            root.right = buildTree(map,map.get(rootValue) +1, right, preOrder);
-
+            root.left = buildTree(inMap, left,  inMap.get(rootValue) - 1, preOrder);
+            root.right = buildTree(inMap, inMap.get(rootValue) + 1, right, preOrder);
             return root;
         } else {
             return null;
