064194a5499e70e377e7988e30cd8aaef5c27d2f
jing1560 <363102403@qq.com>
2024-04-16 14:44:47 +0800

    MaxAreaPutWater

--- src/main/java/com/tech/solution/leetcode/MaxAreaPutWater.java
+++ src/main/java/com/tech/solution/leetcode/MaxAreaPutWater.java
@@ -3,11 +3,22 @@ package com.tech.solution.leetcode;
 /**
  * @author jing1560
  * @data 2024/3/22
- * 【盛最多水的容器】
  *
+ * 算法：盛最多水的容器
+ *
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 返回容器可以储存的最大水量。
+ * 说明：你不能倾斜容器。
+ *
+ * 示例 1：
  * 输入：[1,8,6,2,5,4,8,3,7]
  * 输出：49
  * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+ *
+ * 示例 2：
+ * 输入：height = [1,1]
+ * 输出：1
  */
 public class MaxAreaPutWater {
 
@@ -18,10 +29,13 @@ public class MaxAreaPutWater {
 
         //双指针
         System.out.println("解法3：双指针 -- 最大容量为：" + maxAreaSolution3(new int[]{1,8,6,2,5,4,8,3,7}));
+        System.out.println("解法3：双指针 -- 最大容量为：" + maxAreaSolution3(new int[]{1,1}));
     }
 
     /**
-     解法一：
+     * 解法一：双层循环暴力求解
+     * @param height
+     * @return
      */
     public static int maxAreaSolution1(int[] height){
         if(height.length <= 1){
@@ -30,13 +44,9 @@ public class MaxAreaPutWater {
         int maxArea = 0;
         for(int i = 0; i < height.length; i++){
             for(int j = i+1; j < height.length; j++){
-                if(height[i] <= height[j]){
-                    int tempArea = height[i] * (j - i);
-                    maxArea = Math.max(maxArea,tempArea);
-                }else {
-                    int tempArea = height[j] * (j - i);
-                    maxArea = Math.max(maxArea,tempArea);
-                }
+                int tempHeight = Math.min(height[i], height[j]);
+                int length = j - i;
+                maxArea = Math.max(maxArea, length * tempHeight);
             }
         }
         return maxArea;
@@ -56,13 +66,8 @@ public class MaxAreaPutWater {
             if(left == height.length){
                 break;
             }
-            if(height[left] <= height[right]){
-                int tempArea = height[left] * (right - left);
-                maxArea = Math.max(maxArea,tempArea);
-            }else {
-                int tempArea = height[right] * (right - left);
-                maxArea = Math.max(maxArea,tempArea);
-            }
+            int tempArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea,tempArea);
 
             if(right == height.length - 1){
                 left++;
