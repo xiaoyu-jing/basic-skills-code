953c670427a4bb82f08d6a74ddc08edaf5e18f5b
jing1560 <363102403@qq.com>
2024-04-16 17:24:56 +0800

    SwapParisListNode

--- src/main/java/com/tech/solution/leetcode/SwapParisListNode.java
+++ src/main/java/com/tech/solution/leetcode/SwapParisListNode.java
@@ -6,8 +6,19 @@ package com.tech.solution.leetcode;
  *
  * 算法：两两交换链表中的节点
  *
- * 示例：输入：{1,2,3,4}；
- *      输出：{2,1,4,3}
+ * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+ *
+ * 示例 1：
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ *
+ * 示例 2：
+ * 输入：head = []
+ * 输出：[]
+ *
+ * 示例 3：
+ * 输入：head = [1]
+ * 输出：[1]
  */
 public class SwapParisListNode {
 
@@ -37,12 +48,23 @@ public class SwapParisListNode {
     }
 
     public static void main(String[] args){
-        ListNode node = swapParis(buildNode(new int[]{1,2,3,4}, 0));
-        System.out.println(node.toString());
+        ListNode node = swapParis1(buildNode(new int[]{1,2,3,4}, 0));
+        System.out.println("解法一：" + node.toString());
+
+        ListNode node2 = swapParis2(buildNode(new int[]{1,2,3,4}, 0));
+        System.out.println("解法二：" + node2.toString());
+
+        ListNode node3 = swapParis2(buildNode(new int[]{1}, 0));
+        System.out.println("解法二：" + node3.toString());
 
     }
 
-    private static ListNode swapParis(ListNode head){
+    /**
+     * 解法一：暴力交换 （值发生变化了，不满足题意，但是能解出来）
+     * @param head
+     * @return
+     */
+    private static ListNode swapParis1(ListNode head){
         if(head != null && head.next == null){
             return head;
         }
@@ -68,4 +90,20 @@ public class SwapParisListNode {
         node.next = buildNode(arr,index+1);
         return node;
     }
+
+    /**
+     * 解法二：递归
+     * @param head
+     * @return
+     */
+    private static ListNode swapParis2(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapParis2(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+
 }
