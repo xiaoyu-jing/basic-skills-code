0b5f93849c705f396fdf0ac1c3d07f1089397910
jing1560 <363102403@qq.com>
2024-04-16 17:43:13 +0800

    TreeNodeFindData

--- src/main/java/com/tech/solution/leetcode/TreeNodeFindData.java
+++ src/main/java/com/tech/solution/leetcode/TreeNodeFindData.java
@@ -7,6 +7,16 @@ import java.util.*;
  * @data 2024/3/28
  *
  * 算法：二叉搜索树中第K小的元素
+ *
+ * 给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
+ *
+ * 示例 1：
+ * 输入：root = [3,1,4,null,2], k = 1
+ * 输出：1
+ *
+ * 示例 2：
+ * 输入：root = [5,3,6,2,4,null,null,1], k = 3
+ * 输出：3
  */
 public class TreeNodeFindData {
 
