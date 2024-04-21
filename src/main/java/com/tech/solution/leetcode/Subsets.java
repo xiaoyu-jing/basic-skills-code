376a043ec42ba5206c548bbb35fe4d879117f8be
jing1560 <363102403@qq.com>
2024-04-21 21:51:54 +0800

    subsets Coding

--- src/main/java/com/tech/solution/leetcode/Subsets.java
+++ src/main/java/com/tech/solution/leetcode/Subsets.java
@@ -1,5 +1,8 @@
 package com.tech.solution.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author jing1560
  * @data 2024/4/21
@@ -10,5 +13,25 @@ public class Subsets {
 
     public static void main(String[] args){
 
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        backtrack(nums, 0);
+        return resultList;
+    }
+
+    List<List<Integer>> resultList = new ArrayList<>();
+    List<Integer> list = new ArrayList<>();
+
+    public void backtrack(int[] nums, int start){
+        resultList.add(new ArrayList<>(list));
+        if(start >= nums.length){
+            return;
+        }
+        for(int i = start; i < nums.length; i++){
+            list.add(nums[i]);
+            backtrack(nums, i + 1);
+            list.remove(list.size() - 1);
+        }
     }
 }
