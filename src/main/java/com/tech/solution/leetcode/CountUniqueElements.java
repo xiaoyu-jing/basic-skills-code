ea8b2ff95b4bb2b646e750fac3194f0a87e13674
jing1560 <363102403@qq.com>
2024-04-28 22:45:06 +0800

    countUniqueElements1

--- src/main/java/com/tech/solution/leetcode/CountUniqueElements.java
+++ src/main/java/com/tech/solution/leetcode/CountUniqueElements.java
@@ -15,9 +15,9 @@ package com.tech.solution.leetcode;
 public class CountUniqueElements {
 
     public static void main(String[] args){
-        System.out.println(countUniqueElements2(new int[]{1,2,3,4,5,4,3,2,1})); // 输出 5
+        System.out.println(countUniqueElements1(new int[]{1,2,3,4,5,4,3,2,1})); // 输出 5
 
-        System.out.println(countUniqueElements2(new int[]{2,2,6,11,20,20,25,30,37,35,33,33,30,20,12,3})); // 输出 11
+        System.out.println(countUniqueElements1(new int[]{2,2,6,11,20,20,25,30,37,35,33,33,30,20,12,3})); // 输出 11
     }
 
     /**
@@ -37,54 +37,28 @@ public class CountUniqueElements {
             if(nums[left] == nums[right]){
                 sum++;
                 int temp = nums[left];
+                // 左右的元素相同时
+                // 判断右边是否还存在 重复元素，依次过滤掉
                 while(left <= right && nums[right] == temp)
                     right--;
+                // 判断左边是否还存在 重复元素，依次过滤掉
                 while(left <= right && nums[left] == temp)
                     left++;
             } else if(nums[left] < nums[right]){
                 sum++;
                 int temp = nums[left];
+                // 右边比左边大时，判断左边是否还存在 重复元素，依次过滤掉
                 while(left <= right && nums[left] == temp)
                     left++;
             } else{
                 sum++;
                 int temp = nums[right];
+                // 左边比右边大时，判断右边是否还存在 重复元素，依次过滤掉
                 while(left <= right && nums[right] == temp)
                     right--;
             }
         }
         return sum;
-    }
-
-    /**
-     * 解法 2：牛客的网友答案
-     *
-     * 双指针法，每次取最小元素移动，不使用额外空间，O(n)
-     *
-     * {2,2,6,11,20,20,25,30,37,35,33,33,30,20,12,3}
-     * @param nums
-     * @return
-     */
-    public static int countUniqueElements2(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int l = 0, r = nums.length - 1;
-        int count = 0;
-        while (l <= r) {
-            if (nums[l] < nums[r]) {
-                count++;
-                l++;
-            } else if (nums[l] > nums[r]) {
-                count++;
-                r--;
-            } else {
-                l++;
-                r--;
-                count++;
-            }
-        }
-        return count;
     }
 
 }
