73299d02413f977fade1a2360273e1da65a7ee10
jing1560 <363102403@qq.com>
2024-05-02 23:49:47 +0800

    利用二维矩阵模拟

--- src/main/java/com/tech/solution/leetcode/ZFormalTransformation.java
+++ src/main/java/com/tech/solution/leetcode/ZFormalTransformation.java
@@ -48,6 +48,10 @@ public class ZFormalTransformation {
         System.out.println(zformalConvertSolution1("PAYPALISHIRING", 3));
         System.out.println(zformalConvertSolution1("PAYPALISHIRING", 4));
         System.out.println(zformalConvertSolution1("A", 1));
+
+        System.out.println(zformalConvertSolution2("PAYPALISHIRING", 3));
+        System.out.println(zformalConvertSolution2("PAYPALISHIRING", 4));
+        System.out.println(zformalConvertSolution2("A", 1));
     }
 
     /**
@@ -82,6 +86,55 @@ public class ZFormalTransformation {
         for(StringBuilder row : res){
             builder.append(row);
         }
+        return builder.toString();
+    }
+
+    /**
+     * 解法二：官方解法   利用二维矩阵模拟
+     * @param s
+     * @param numRows
+     * @return
+     */
+    private static String zformalConvertSolution2(String s, int numRows){
+        int n = s.length();
+        if(numRows == 1 || numRows >= n){
+            return s;
+        }
+        // 在矩阵上填写字符时，会向下填写 r 个字符，然后向右上继续填写 r−2 个字符，最后回到第一行，因此 Z 字形变换的周期 t=r+r−2=2r−2
+        // 2*numRows−2 表示一个 Z 字形周期中有几个字符
+        int cycleNum = 2 * numRows - 2;
+        // 每个周期会占用矩阵上的 1+numRows−2= numRows−1 列
+        // (n / cycleNum) 表示有多少个周期；(numRows - 1) 表示 一个周期有几列； 乘积表示所有周期共有几列
+        //int column = (n / cycleNum) * (numRows - 1) + 1;
+        // (n+t-1)/t * (r-1): 要把最后一个周期视作一个完整周期，所以要+t-1，可以当作是要让它向上取整而不是向下取整. "-1" 是因为要排除 n 正好被周期整除的时候
+        int column = (n + cycleNum - 1) / cycleNum * (numRows - 1);
+        // 二维数组存储字符串构造数据
+        char[][] resultArr = new char[numRows][column];
+        // 上下移动
+        int upX = 0;
+        // 右上移动
+        int rightY = 0;
+        for(int i = 0; i < n; i++){
+            resultArr[upX][rightY] = s.charAt(i);
+            // (numRows - 1) 表示 一个周期有几列；
+            // i % cycleNum  表示 i 处于 一个周期中的第几列
+            if(i % cycleNum < numRows - 1){
+                upX++; // 向下移动
+            } else {
+                upX--;
+                rightY++;  // 向右上移动
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for(char[] rowValue : resultArr){
+            for(char columnValue : rowValue){
+                if(columnValue != 0){
+                    builder.append(columnValue);
+                }
+            }
+        }
+
         return builder.toString();
     }
 }
