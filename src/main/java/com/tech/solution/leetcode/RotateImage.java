d06ab6443da3eb4c89fe033b5e05cd3a3450f6ea
jing1560 <363102403@qq.com>
2024-05-09 20:02:58 +0800

    旋转图像 - printImage

--- src/main/java/com/tech/solution/leetcode/RotateImage.java
+++ src/main/java/com/tech/solution/leetcode/RotateImage.java
@@ -1,5 +1,7 @@
 package com.tech.solution.leetcode;
 
+import java.util.Arrays;
+
 /**
  * @author jing1560
  * @data 2024/5/9
@@ -23,4 +25,35 @@ package com.tech.solution.leetcode;
  * -1000 <= matrix[i][j] <= 1000
  */
 public class RotateImage {
+
+    public static void main(String[] args){
+        int[][] matrix = {{1,2,3}, {4,5,6},{7,8,9}};
+        rotateImageSolution1(matrix);
+        printImage(matrix);
+    }
+
+    /**
+     * 解法一：使用辅助数组
+     *       规律：对于矩阵中第 i 行的第 j 个元素，在旋转后，它出现在倒数第 i 列的第 j 个位置。
+     */
+    private static void rotateImageSolution1(int[][] matrix){
+        int n = matrix.length;
+        int[][] matrix_new = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                matrix_new[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                matrix[i][j] = matrix_new[i][j];
+            }
+        }
+    }
+
+    private static void printImage(int[][] matrix){
+        for(int i = 0; i < matrix.length; i++){
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+    }
 }
