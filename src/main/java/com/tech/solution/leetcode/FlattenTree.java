e35146efcdff778f897c9286aa1b3fd5a8f91c57
jing1560 <363102403@qq.com>
2024-05-15 11:41:11 +0800

    FlattenTree

---
+++ src/main/java/com/tech/solution/leetcode/FlattenTree.java
@@ -0,0 +1,26 @@
+package com.tech.solution.leetcode;
+
+/**
+ * @author jing1560
+ * @data 2024/5/15
+ *
+ * 算法：二叉树展开为链表
+ *
+ * 给你二叉树的根结点 root ，请你将它展开为一个单链表：
+ * 1）展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+ * 2）展开后的单链表应该与二叉树 先序遍历 顺序相同。
+ *
+ * 示例 1：
+ * 输入：root = [1,2,5,3,4,null,6]
+ * 输出：[1,null,2,null,3,null,4,null,5,null,6]
+ *
+ * 示例 2：
+ * 输入：root = []
+ * 输出：[]
+ *
+ * 示例 3：
+ * 输入：root = [0]
+ * 输出：[0]
+ */
+public class FlattenTree {
+}
